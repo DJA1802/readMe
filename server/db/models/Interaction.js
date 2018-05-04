623419198678c058ecc5593bc1f67ca9c7b90411
Jeff Gore <jgore00@gmail.com>
2018-05-04 15:34:01 -0400

    Added Interaction model, updated package.json to reflect our project name/description, filled in missing .babelrc data

---
+++ server/db/models/Interaction.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Interaction = db.define('interaction', {
+  startTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  endTime: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  duration: {
+    type: Sequelize.VIRTUAL(Sequelize.INTEGER),
+    get () {
+      return this.getDataValue('endTime') - this.getDataValue('startTime');
+    }
+  }
+});
+
+module.exports = Interaction;
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,20 +5,11 @@ const Topic = require('./Topic');
 const Author = require('./Author');
 const Publication = require('./Publication');
 const PublicationType = require('./PublicationType');
+const Interaction = require('./Interaction');
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Interaction.belongsTo(Article);
+Article.hasMany(Interaction);
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
 module.exports = {
   User,
   Article,
