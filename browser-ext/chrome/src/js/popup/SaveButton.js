ce666c78f7e777925814324eccde5d8c994e6340
Don Leistman <donleistman@gmail.com>
2018-05-08 17:17:53 -0400

    integrate semantic ui with browser ext

--- browser-ext/chrome/src/js/popup/SaveButton.js
+++ browser-ext/chrome/src/js/popup/SaveButton.js
@@ -1,16 +1,17 @@
-import React from "react";
-import axios from "axios";
-import { hot } from "react-hot-loader";
-import { Button } from "semantic-ui-react";
+import React from 'react';
+import axios from 'axios';
+// import 'semantic-ui-css/semantic.css';
+import { hot } from 'react-hot-loader';
+import { Button } from 'semantic-ui-react';
 
 const clickHander = () => {
-  console.log("you clicked the button!");
-  chrome.tabs.getSelected(null, function(tab) {
+  console.log('you clicked the button!');
+  chrome.tabs.getSelected(null, function (tab) {
     var articleUrl = tab.url;
     console.log(articleUrl);
     axios
-      .post("http://localhost:8080/api/articles", { articleUrl })
-      .then(() => console.log("request posted"));
+      .post('http://localhost:8080/api/articles', { articleUrl })
+      .then(() => console.log('request posted'));
   });
 };
 
