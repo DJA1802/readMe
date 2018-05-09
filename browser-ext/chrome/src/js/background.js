be447f2b9ba115f73cccea676725c1b37b26b1ed
Anurag Prasad <anuyorker@gmail.com>
2018-05-09 12:10:34 -0400

    before modifying chrome extension to execute script on click

--- browser-ext/chrome/src/js/background.js
+++ browser-ext/chrome/src/js/background.js
@@ -2,5 +2,5 @@ import '../img/icon-128.png';
 import '../img/icon-34.png';
 
 chrome.browserAction.onClicked.addListener(function () {
-  chrome.tabs.create({ url: 'index.html' });
+  chrome.tabs.create({ url: 'background.html' });
 });
