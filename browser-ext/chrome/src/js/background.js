7127c8bdb8692030d5f89536236b4e9bd045fa0b
Anurag Prasad <anuyorker@gmail.com>
2018-05-10 11:44:03 -0400

    background.js runs script on chrome extension click

--- browser-ext/chrome/src/js/background.js
+++ browser-ext/chrome/src/js/background.js
@@ -2,21 +2,49 @@ import '../img/icon-128.png';
 import '../img/icon-34.png';
 import axios from 'axios';
 
+const getArticleUrl = () => chrome.tabs.getSelected(null, tab => tab.url);
+
+// const fulfillSave = () => {
+//   console.log('Saving article');
+//   axios
+//     .post('http://localhost:8080/api/articles', { articleUrl })
+//     .then(() => console.log('request posted'));
+// };
+
+chrome.extension.onMessage.addListener((request, sender) => {
+  chrome.tabs.update(sender.tab.id, { url: request.redirect });
+});
+
 chrome.browserAction.onClicked.addListener(tab => {
   // detect session from web app
   axios
     .get('http://localhost:8080/auth/me', {
-      withcredentials: 'include'
+      withCredentials: 'include'
     })
     .then(data => {
+      const articleUrl = getArticleUrl();
       if (data.data) {
+        console.log('User is logged in.');
         // logged in
-        // make request to add article to this user's articles
+        // (A) make request to add article to this user's articles (need to do this by userId?)
+        // fullfillSave(articleUrl);
+        // Content Script to display "Saved Article" ?
       } else {
+        console.log('User is not logged in.');
         // not logged in
-        // redirect to login page
+        // (B) redirect to login page
+        // chrome.windows.create({
+        //   url: 'http:/localhost:8080/auth/login',
+        //   type: 'popup',
+        //   focused: true
+        // });
+        chrome.extension.sendMessage({
+          redirect: 'http:/localhost:8080/auth/login'
+        });
+
+        // after successful login, go back to original url and fulfill (A)
+        // fulfillSave(articleUrl);
       }
-      console.log('data ==>', data.data);
     })
     .catch(err => console.log(err));
 });
