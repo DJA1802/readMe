ef83538c446c65d780fbc20b60c27e93097a79ab
Anurag Prasad <anuyorker@gmail.com>
2018-05-10 17:23:23 -0400

    before merging localStorage stuff

--- browser-ext/chrome/src/js/background.js
+++ browser-ext/chrome/src/js/background.js
@@ -4,16 +4,29 @@ import axios from 'axios';
 
 const getArticleUrl = () => chrome.tabs.getSelected(null, tab => tab.url);
 
-// const fulfillSave = () => {
-//   console.log('Saving article');
-//   axios
-//     .post('http://localhost:8080/api/articles', { articleUrl })
-//     .then(() => console.log('request posted'));
-// };
+const fulfillSave = (articleUrl) => {
+  console.log('Saving article');
+  // axios
+  //   .post('http://localhost:8080/api/articles', { articleUrl })
+  //   .then(() => console.log('request posted'));
 
-chrome.extension.onMessage.addListener((request, sender) => {
-  chrome.tabs.update(sender.tab.id, { url: request.redirect });
-});
+  chrome.extension.onMessage.addListener(
+    (request, sender, sendResponse) => {
+      // Create a simple text notification:
+      if (chrome.notifications.getPermissionLevel() == 'denied') {
+        console.log('Browser notification permission denied');
+      } else {
+        chrome.notifications.create({
+          options: {
+            type: 'basic',
+            message: request.msg,
+            eventTime: 1
+          }
+        });
+      }
+    }
+  );
+};
 
 chrome.browserAction.onClicked.addListener(tab => {
   // detect session from web app
@@ -24,11 +37,13 @@ chrome.browserAction.onClicked.addListener(tab => {
     .then(data => {
       const articleUrl = getArticleUrl();
       if (data.data) {
+        console.log('User data ==>', data.data);
         console.log('User is logged in.');
         // logged in
         // (A) make request to add article to this user's articles (need to do this by userId?)
-        // fullfillSave(articleUrl);
-        // Content Script to display "Saved Article" ?
+        fullfillSave(articleUrl);
+        // Content Script to display "Saved Article"
+
       } else {
         console.log('User is not logged in.');
         // not logged in
@@ -38,10 +53,7 @@ chrome.browserAction.onClicked.addListener(tab => {
         //   type: 'popup',
         //   focused: true
         // });
-        chrome.extension.sendMessage({
-          redirect: 'http:/localhost:8080/auth/login'
-        });
-
+        chrome.tabs.create({ url: 'http://localhost:8080/auth/login' });
         // after successful login, go back to original url and fulfill (A)
         // fulfillSave(articleUrl);
       }
