74d11ca49d069112d6da34c9350961850646649f
Don Leistman <donleistman@gmail.com>
2018-05-11 00:34:57 -0400

    add archive / delete icons to article card

--- web-app/client/components/ArticleCard.js
+++ web-app/client/components/ArticleCard.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Icon, Image } from 'semantic-ui-react';
 
-const ArticleCard = props => {
-  const { articleId, title } = props;
-  return (
-    <Card className="article-card" as={Link} to={`/articles/${articleId}`}>
-      <Image src="http://fillmurray.com/200/200" />
-      <Card.Content>
-        <Card.Header>{title}</Card.Header>
-      </Card.Content>
-    </Card>
-  );
-};
+class ArticleCard extends Component {
+  state = { hover: false };
+
+  handleMouseEnter = () => {
+    console.log('mouse enter!');
+    this.setState({ hover: true });
+  };
+
+  handleMouseLeave = () => {
+    console.log('mouse leave!');
+    this.setState({ hover: false });
+  };
+
+  render () {
+    const { articleId, title } = this.props;
+    return (
+      <Card
+        className="article-card"
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        raised={this.state.hover && true}
+        link
+      >
+        <Image
+          // src="http://fillmurray.com/200/200"
+          style={{ backgroundImage: `url(http://fillmurray.com/300/200)` }}
+          className="article-card-image"
+          as={Link}
+          to={`/articles/${articleId}`}
+        />
+        <Card.Content header={title} as={Link} to={`/articles/${articleId}`} />
+
+        <Card.Content extra className="card-icon-container">
+          {this.state.hover && (
+            <React.Fragment>
+              <Icon
+                name="archive"
+                size="large"
+                onClick={() => console.log('click')}
+                link
+              />
+              <Icon
+                name="trash outline"
+                size="large"
+                onClick={() => console.log('click')}
+                link
+              />
+            </React.Fragment>
+          )}
+        </Card.Content>
+      </Card>
+    );
+  }
+}
 
 export default ArticleCard;
