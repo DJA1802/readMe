7f8dae37276c10cd3f346fe844a966a3e3f238b4
Jeff Gore <jgore00@gmail.com>
2018-05-12 19:43:02 -0400

    Merged Dons latest changes and integrated eager-loading query into refactored article

--- web-app/server/api/articles.js
+++ web-app/server/api/articles.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const request = require('request');
+const request = require('request-promise-native');
 const { Article, Author, Publication } = require('../db/models');
 const { setPublicationName, buildMercuryJSONRequest } = require('../utils');
 
@@ -26,53 +26,56 @@ router.get('/', (req, res, next) => {
 });
 
 // POST /api/articles
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   const { articleUrl } = req.body;
   const userId = req.user.id;
-
-  request({ url: articleUrl }, (htmlErr, htmlRes, htmlStr) => {
-    if (htmlErr) console.log(htmlErr);
-    const publicationName = setPublicationName(htmlStr, articleUrl);
-    return Publication.findOrCreate({
-      where: {
-        name: publicationName
-      }
-    }).then(([publication]) => {
-      request(
-        buildMercuryJSONRequest(articleUrl),
-        (apiErr, apiRes, apiBody) => {
-          if (apiErr) console.log(apiErr);
-          const data = JSON.parse(apiBody);
-          Article.create({
-            title: data.title,
-            sourceUrl: data.url,
-            content: data.content,
-            wordCount: data.wordCount,
-            publicationDate: data.publicationDate,
-            userId,
-            publicationId: publication.id
-          })
-            .then(newArticle =>
-              Article.findOne({
-                where: { id: newArticle.id },
-                attributes: [
-                  'id',
-                  'title',
-                  'sourceUrl',
-                  'status',
-                  'wordCount',
-                  'status',
-                  'createdAt'
-                ],
-                include: [{ model: Author }, { model: Publication }]
-              })
-            )
-            .then(newArticle => res.status(201).json(newArticle))
-            .catch(next);
-        }
-      );
-    });
+  const htmlStr = await request({ url: articleUrl }).catch(err => {
+    next(err);
   });
+  const publicationName = setPublicationName(htmlStr, articleUrl);
+  const publication = await Publication.findOrCreate({
+    where: {
+      name: publicationName
+    }
+  }).catch(err => {
+    next(err);
+  });
+  const mercuryResponse = await request(
+    buildMercuryJSONRequest(articleUrl)
+  ).catch(err => {
+    next(err);
+  });
+  const mercuryArticle = JSON.parse(mercuryResponse);
+
+  const newArticle = await Article.create({
+    title: mercuryArticle.title,
+    sourceUrl: mercuryArticle.url,
+    content: mercuryArticle.content,
+    wordCount: mercuryArticle.wordCount,
+    publicationDate: mercuryArticle.publicationDate,
+    userId,
+    publicationId: publication.id
+  }).catch(err => {
+    next(err);
+  });
+
+  const associatedArticle = await Article.findOne({
+    where: { id: newArticle.id },
+    attributes: [
+      'id',
+      'title',
+      'sourceUrl',
+      'status',
+      'wordCount',
+      'status',
+      'createdAt'
+    ],
+    include: [{ model: Author }, { model: Publication }]
+  }).catch(err => {
+    next(err);
+  });
+
+  res.status(201).json(associatedArticle);
 });
 
 // GET /api/articles/:id
