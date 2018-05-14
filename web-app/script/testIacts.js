1781a7e72369eb0b72ad40c8c80c24160c5fcbff
Jeff Gore <jgore00@gmail.com>
2018-05-14 17:19:00 -0400

    Added Article.getMostReadByTime() class method

--- web-app/script/testIacts.js
+++ web-app/script/testIacts.js
@@ -1,15 +1,16 @@
 const db = require('../server/db');
-const { Interaction } = require('../server/db/models');
+const { Interaction, Article } = require('../server/db/models');
 
-Interaction.getAverageLength(1).then(userAvgTime =>
-  console.log('Average in milliseconds: ', userAvgTime)
-);
-Interaction.getAverageLength(1, 'seconds').then(userAvgTime =>
-  console.log('Average in seconds: ', userAvgTime)
-);
-Interaction.getAverageLength(1, 'minutes').then(userAvgTime =>
-  console.log('Average in minutes: ', userAvgTime)
-);
-Interaction.getAverageLength(1, 'hours').then(userAvgTime =>
-  console.log('Average in hours: ', userAvgTime)
-);
+// Interaction.getAverageLength(1).then(userAvgTime =>
+//   console.log('Average in milliseconds: ', userAvgTime)
+// );
+// Interaction.getAverageLength(1, 'seconds').then(userAvgTime =>
+//   console.log('Average in seconds: ', userAvgTime)
+// );
+// Interaction.getAverageLength(1, 'minutes').then(userAvgTime =>
+//   console.log('Average in minutes: ', userAvgTime)
+// );
+// Interaction.getAverageLength(1, 'hours').then(userAvgTime =>
+//   console.log('Average in hours: ', userAvgTime)
+// );
+Article.getMostReadByTime().then(console.log);
--- web-app/server/db/models/Article.js
+++ web-app/server/db/models/Article.js
@@ -58,4 +58,14 @@ Article.beforeCreate(articleInstance => {
   // set some tags on this article
 });
 
+Article.getMostReadByTime = function () {
+  return db
+    .query(
+      'SELECT articles.id, articles.title, articles."sourceUrl", articles."publicationDate", articles."wordCount", articles.status, articles."publicationId", SUM(EXTRACT(EPOCH FROM interactions."endTime"-interactions."startTime"))*1000 AS "duration" FROM articles INNER JOIN interactions on articles.id = interactions."articleId" GROUP BY articles.id, articles.title, articles."sourceUrl", articles."publicationDate", articles."wordCount", articles.status, articles."publicationId" ORDER BY "duration";'
+    )
+    .then(data => {
+      return data[0].length ? data[0][0] : null;
+    });
+};
+
 module.exports = Article;
