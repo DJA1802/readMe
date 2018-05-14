ea1531f4248518a4d3fc9a9713eaacf9390dab62
Don Leistman <donleistman@gmail.com>
2018-05-13 23:44:24 -0400

    set up simple chart

--- web-app/client/components/Analytics.js
+++ web-app/client/components/Analytics.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Header, Segment } from 'semantic-ui-react';
-import { VictoryPie } from 'victory';
+import { VictoryAxis, VictoryArea, VictoryChart, VictoryTheme } from 'victory';
+import moment from 'moment';
 import { fetchInteractions } from '../store';
 
 class Analytics extends Component {
@@ -11,12 +12,25 @@ class Analytics extends Component {
 
   render () {
     const { interactions } = this.props;
-    console.log(interactions);
+
+    const chartData = interactions.map(interaction => ({
+      date: new Date(interaction.startTime),
+      duration: interaction.duration
+    }));
+    console.log('chartData', chartData);
+
     return (
       <div id="analytics-container">
         <Header as="h1">Analytics</Header>
         <Segment id="chart-container">
-          <VictoryPie id="chart" />
+          <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
+            <VictoryAxis tickFormat={x => moment(x).format('MMM D YYYY')} />
+            <VictoryAxis
+              dependentAxis
+              tickFormat={y => `${Math.floor(y / 60000)} min`}
+            />
+            <VictoryArea id="chart" data={chartData} x="date" y="duration" />
+          </VictoryChart>
         </Segment>
       </div>
     );
