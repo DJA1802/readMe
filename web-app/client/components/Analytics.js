ab2ea7b7bc102047f141289dd5dcd4e5acd13f83
Don Leistman <donleistman@gmail.com>
2018-05-13 21:12:04 -0400

    set up initial styles for chart element; import victory charting library

--- web-app/client/components/Analytics.js
+++ web-app/client/components/Analytics.js
@@ -1,21 +1,38 @@
-// Single Article Page Component (i.e. where article is read from)
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Header, Segment } from 'semantic-ui-react';
+import { VictoryPie } from 'victory';
+import { fetchInteractions } from '../store';
 
-const Analytics = () => {
-  return (
-    <div className="analytics-container">
-      <Header as="h1">Analytics</Header>
-      <Segment />
-    </div>
-  );
-};
+class Analytics extends Component {
+  componentWillMount () {
+    this.props.loadData();
+  }
+
+  render () {
+    const { interactions } = this.props;
+    console.log(interactions);
+    return (
+      <div id="analytics-container">
+        <Header as="h1">Analytics</Header>
+        <Segment id="chart-container">
+          <VictoryPie id="chart" />
+        </Segment>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
-    articles: state.articlesAll
+    interactions: state.interactions
   };
 };
 
-export default connect(mapStateToProps)(Analytics);
+const mapDispatchToProps = dispatch => {
+  return {
+    loadData: () => dispatch(fetchInteractions())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Analytics);
