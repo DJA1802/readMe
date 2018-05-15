a2f63f0a3d804181e4ee2823f59de7d9264a9b16
Jeff Gore <jgore00@gmail.com>
2018-05-14 20:21:10 -0400

    Cleaned up query test file

--- web-app/script/testDbAggQueries.js
+++ web-app/script/testDbAggQueries.js
@@ -1,18 +1,6 @@
 const db = require('../server/db');
 const { Interaction, Article } = require('../server/db/models');
 
-// Interaction.getAverageLength(1).then(userAvgTime =>
-//   console.log('Average in milliseconds: ', userAvgTime)
-// );
-// Interaction.getAverageLength(1, 'seconds').then(userAvgTime =>
-//   console.log('Average in seconds: ', userAvgTime)
-// );
-// Interaction.getAverageLength(1, 'minutes').then(userAvgTime =>
-//   console.log('Average in minutes: ', userAvgTime)
-// );
-// Interaction.getAverageLength(1, 'hours').then(userAvgTime =>
-//   console.log('Average in hours: ', userAvgTime)
-// );
 Article.groupByTimeRead(1).then(data => console.log('groupbyTimeRead ', data));
 Article.getMostReadByDuration(1).then(data =>
   console.log('getMostReadByDuration ', data)
