011743bf9a5c27710cd4e946c9808f8cd0d0dc3f
Don Leistman <donleistman@gmail.com>
2018-05-15 18:07:36 -0400

    remove dead code

--- web-app/service-worker.js
+++ web-app/service-worker.js
@@ -20,17 +20,6 @@ self.addEventListener('fetch', event => {
     caches
       .match(event.request)
       .then(cachedResponse => {
-        // Cache hit - return response
-        // if (cachedResponse && !navigator.onLine) {
-        //   console.log('returning cached response!');
-        //   return cachedResponse;
-        // }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        console.log('ln 33, event from fetch', event);
         const fetchRequest = event.request.clone();
 
         return fetch(fetchRequest)
@@ -57,20 +46,6 @@ self.addEventListener('fetch', event => {
 
               return serverResponse;
             }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            // const responseToCache = serverResponse.clone();
-
-            // caches.open('readme').then(cache => {
-            //   if (event.request.method === 'GET') {
-            //     cache.put(event.request, responseToCache);
-            //   }
-            // });
-
-            // return serverResponse;
           })
           .catch(err => {
             console.log(err);
