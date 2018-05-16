5bbe1bcc23f523d813ed24a1011d5f5254d70640
Jeff Gore <jgore00@gmail.com>
2018-05-16 11:28:22 -0400

    Added /homePageStats route to api/users

--- web-app/client/components/AnalyticsFeatured.js
+++ web-app/client/components/AnalyticsFeatured.js
@@ -3,21 +3,15 @@ import React from 'react';
 import { Container, Header, Statistic } from 'semantic-ui-react';
 import { AnalyticsStat } from '../components';
 
-const AnalyticsFeatured = () => {
-  const dummyData = [
-    { value: '2 hr 3 min', label: 'spent reading\nthis week' },
-    { value: 13, label: 'different\npublications' },
-    { value: '8 hr 1 min', label: 'est. time to\nfinish your list' }
-  ];
-
+const AnalyticsFeatured = props => {
   return (
     <Container className="analytics-featured">
       <Header as="h2" className="nimbus-mono-bold">
         Featured Analytics
       </Header>
       <Statistic.Group widths="three" size="small">
-        {dummyData.map((datum, idx) => (
-          <AnalyticsStat key={idx} value={datum.value} label={datum.label} />
+        {props.map((stat, idx) => (
+          <AnalyticsStat key={idx} value={stat.value} label={stat.label} />
         ))}
       </Statistic.Group>
     </Container>
