629e51fc36777a0a1ee15072580f57a857df2309
Jeff Gore <jgore00@gmail.com>
2018-05-18 01:50:01 -0400

    Restore agg queries, previously commented out

--- web-app/script/testDbAggQueries.js
+++ web-app/script/testDbAggQueries.js
@@ -1,61 +1,53 @@
 const { Interaction, Article, Publication } = require('../server/db/models');
 
-// Interaction.readingTimeByDate(1).then(data =>
-//   console.log('Interaction.readingTimeByDate ', data)
-// );
-// Interaction.readingTimeByMonthYear(1).then(data =>
-//   console.log('Interaction.readingTimeByMonthYear ', data)
-// );
-// Interaction.readingStartTimesByHour(1).then(data =>
-//   console.log('Interaction.readingStartTimesByHour ', data)
-// );
-// Interaction.getAverageLength(1).then(data =>
-//   console.log('Interaction.getAverageLength ', data)
-// );
-// Interaction.getUserFirstEverInteraction(1).then(data =>
-//   console.log('getUserFirstEverInteraction ', data)
-// );
-// Article.getMostReadByDuration(1).then(data =>
-//   console.log('getMostReadByDuration ', data)
-// );
-// Article.getAverageWordCount(1).then(data =>
-//   console.log('getAverageWordCount ', data)
-// );
-// Article.getTotalWordCount(1).then(data =>
-//   console.log('getTotalWordCount ', data)
-// );
-// Article.getTotalWordCount(1, ['my-list']).then(data =>
-//   console.log('getTotalWordCount my-list only ', data)
-// );
-// Interaction.readingTimeThisX(1, 'week').then(data => {
-//   console.log('readingTimeThisWeek ', data);
-// });
-// Interaction.readingTimeThisX(1, 'week', false).then(data => {
-//   console.log('readingTimeThisWeek numeric ', data);
-// });
-// Interaction.readingTimeThisX(1, 'month').then(data => {
-//   console.log('readingTimeThisMonth ', data);
-// });
-// Interaction.readingTimeThisX(1, 'year').then(data => {
-//   console.log('readingTimeThisYear ', data);
-// });
-// Publication.getDistinctForUser(1).then(data => {
-//   console.log('Publication.getDistinctForUser ', data);
-// });
-// Publication.groupByArticleCount(1).then(data => {
-//   console.log('Publication.groupByArticleCount ', data);
-// });
-// Interaction.getAllForUser(1).then(data => {
-//   console.log(
-//     'Article.findAllForUser ',
-//     data.map(datum => datum.article.title)
-//   );
-// });
-
-Article.groupByTimeRead(1, ['my-list', 'archive'], true).then(data =>
-  console.log('Article.groupByTimeRead ', data)
+Interaction.readingTimeByDate(1).then(data =>
+  console.log('Interaction.readingTimeByDate ', data)
 );
-
-Article.groupByInteractionCount(1, ['my-list', 'archive'], true).then(data =>
-  console.log('Article.groupByTimeRead ', data)
+Interaction.readingTimeByMonthYear(1).then(data =>
+  console.log('Interaction.readingTimeByMonthYear ', data)
 );
+Interaction.readingStartTimesByHour(1).then(data =>
+  console.log('Interaction.readingStartTimesByHour ', data)
+);
+Interaction.getAverageLength(1).then(data =>
+  console.log('Interaction.getAverageLength ', data)
+);
+Interaction.getUserFirstEverInteraction(1).then(data =>
+  console.log('getUserFirstEverInteraction ', data)
+);
+Article.getMostReadByDuration(1).then(data =>
+  console.log('getMostReadByDuration ', data)
+);
+Article.getAverageWordCount(1).then(data =>
+  console.log('getAverageWordCount ', data)
+);
+Article.getTotalWordCount(1).then(data =>
+  console.log('getTotalWordCount ', data)
+);
+Article.getTotalWordCount(1, ['my-list']).then(data =>
+  console.log('getTotalWordCount my-list only ', data)
+);
+Interaction.readingTimeThisX(1, 'week').then(data => {
+  console.log('readingTimeThisWeek ', data);
+});
+Interaction.readingTimeThisX(1, 'week', false).then(data => {
+  console.log('readingTimeThisWeek numeric ', data);
+});
+Interaction.readingTimeThisX(1, 'month').then(data => {
+  console.log('readingTimeThisMonth ', data);
+});
+Interaction.readingTimeThisX(1, 'year').then(data => {
+  console.log('readingTimeThisYear ', data);
+});
+Publication.getDistinctForUser(1).then(data => {
+  console.log('Publication.getDistinctForUser ', data);
+});
+Publication.groupByArticleCount(1).then(data => {
+  console.log('Publication.groupByArticleCount ', data);
+});
+Interaction.getAllForUser(1).then(data => {
+  console.log(
+    'Article.findAllForUser ',
+    data.map(datum => datum.article.title)
+  );
+});
