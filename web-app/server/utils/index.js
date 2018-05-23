68a3bb2ac9ef6fd08e3fade06b8389147f01bec2
Jeff Gore <jgore00@gmail.com>
2018-05-23 15:36:31 -0400

    Fixed import/export from server/utils; was incorrectly using front-end "export" syntax

--- web-app/server/utils/index.js
+++ web-app/server/utils/index.js
@@ -1,5 +1,17 @@
-export * from './calcsAndConversions';
-export * from './extraction';
-export * from './formatting';
-export * from './gatekeeperMiddleware';
-export * from './optionsObjects';
+const { convertMilliseconds, average } = require('./calcsAndConversions');
+const { setPublicationName, extractSrcAttribute } = require('./extraction');
+const { sqlInList, msToTime, formatDuration } = require('./formatting');
+const { isLoggedIn } = require('./gatekeeperMiddleware');
+const { buildMercuryJSONRequest } = require('./optionsObjects');
+
+module.exports = {
+  convertMilliseconds,
+  average,
+  setPublicationName,
+  extractSrcAttribute,
+  sqlInList,
+  msToTime,
+  formatDuration,
+  isLoggedIn,
+  buildMercuryJSONRequest
+};
