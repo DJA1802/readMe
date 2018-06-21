fb749264ce2d578becd320379c3ed945c5501f41
Jeff Gore <jgore00@gmail.com>
2018-06-21 17:26:00 -0400

    fix placeholder values in login and signup form

--- web-app/client/components/FieldWithWarning.js
+++ web-app/client/components/FieldWithWarning.js
@@ -3,12 +3,12 @@ import { Input } from 'semantic-ui-react';
 
 const FieldWithWarning = ({
   input,
-  label,
+  placeholder,
   type,
   meta: { touched, error, warning }
 }) => (
   <React.Fragment>
-    <Input {...input} placeholder={label} type={type} />
+    <Input {...input} placeholder={placeholder} type={type} />
     {touched && ((error && <p>{error}</p>) || (warning && <p>{warning}</p>))}
   </React.Fragment>
 );
