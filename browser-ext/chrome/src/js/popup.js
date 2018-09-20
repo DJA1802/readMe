b80bebefd1d461eb999b95c81b4a64cff0c17c20
Don Leistman <donleistman@gmail.com>
2018-09-19 23:28:16 -0400

    deleted chrome extension folder and moved web app into repo's root directory. Chrome ext has been spun off into another repository

--- browser-ext/chrome/src/js/popup.js
+++
@@ -1,6 +0,0 @@
-import '../css/popup.css';
-import Popup from './popup/index.js';
-import React from 'react';
-import { render } from 'react-dom';
-
-render(<Popup />, window.document.getElementById('app-container'));
