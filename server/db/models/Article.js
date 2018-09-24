61655a19d044f9bddcda3641d2c0558502e5f819
Jeff Gore <jgore00@gmail.com>
2018-09-24 15:09:05 -0400

    add line breaks to SQL queries in db/models/Article for clarity

--- server/db/models/Article.js
+++ server/db/models/Article.js
@@ -108,11 +108,42 @@ Article.groupByTimeRead = function (
 ) {
   return db
     .query(
-      `SELECT articles.id, articles.title, articles."sourceUrl", articles."publicationDate", articles."wordCount", articles.status, articles."publicationId", articles."deletedAt", SUM(EXTRACT(EPOCH FROM interactions."endTime"-interactions."startTime")*1000) AS "duration" FROM articles INNER JOIN interactions on articles.id = interactions."articleId" INNER JOIN users ON articles."userId" = users.id WHERE users.id = ${userId} AND articles.status IN ${sqlInList(
-        articleTypes
-      )} ${
-        includeDeleted ? '' : 'AND articles."deletedAt" IS NULL'
-      } GROUP BY articles.id, articles.title, articles."sourceUrl", articles."publicationDate", articles."wordCount", articles.status, articles."publicationId" ORDER BY "duration" DESC;`
+      `SELECT
+        articles."id"
+      , articles."title"
+      , articles."sourceUrl"
+      , articles."publicationDate"
+      , articles."wordCount"
+      , articles."status"
+      , articles."publicationId"
+      , articles."deletedAt"
+      , SUM(
+          EXTRACT(EPOCH FROM interactions."endTime" - interactions."startTime")
+          * 1000
+        ) AS "duration"
+      FROM
+        articles
+      INNER JOIN
+        interactions
+        ON articles."id" = interactions."articleId"
+      INNER JOIN
+        users
+        ON articles."userId" = users."id"
+      WHERE
+        users."id" = ${userId}
+        AND articles."status" IN ${sqlInList(articleTypes)}
+        ${includeDeleted ? '' : 'AND articles."deletedAt" IS NULL'}
+      GROUP BY
+        articles."id"
+      , articles."title"
+      , articles."sourceUrl"
+      , articles."publicationDate"
+      , articles."wordCount"
+      , articles."status"
+      , articles."publicationId"
+      ORDER BY
+        "duration" DESC
+      ;`
     )
     .then(data => {
       return data[0]
@@ -148,11 +179,32 @@ Article.groupByInteractionCount = function (
 ) {
   return db
     .query(
-      `SELECT articles.id, articles.title, articles."sourceUrl", articles."publicationDate", articles."wordCount", articles.status,  articles."deletedAt", COUNT(interactions."startTime")::integer AS "interactionCount" FROM articles INNER JOIN interactions on articles.id = interactions."articleId" INNER JOIN users ON articles."userId" = users.id WHERE users.id = ${userId} AND articles.status IN ${sqlInList(
-        articleTypes
-      )} ${
-        includeDeleted ? '' : 'AND articles."deletedAt" IS NULL'
-      } GROUP BY articles.id ORDER BY "interactionCount" DESC;`
+      `SELECT
+        articles."id"
+      , articles."title"
+      , articles."sourceUrl"
+      , articles."publicationDate"
+      , articles."wordCount"
+      , articles."status"
+      , articles."deletedAt"
+      , COUNT(interactions."startTime")::integer AS "interactionCount"
+      FROM
+        articles
+      INNER JOIN
+        interactions
+        on articles."id" = interactions."articleId"
+      INNER JOIN
+        users
+        ON articles."userId" = users."id"
+      WHERE
+        users."id" = ${userId}
+        AND articles."status" IN ${sqlInList(articleTypes)}
+        ${includeDeleted ? '' : 'AND articles."deletedAt" IS NULL'}
+      GROUP BY
+        articles."id"
+      ORDER BY
+        "interactionCount" DESC
+      ;`
     )
     .then(data => data[0]);
 };
@@ -164,9 +216,17 @@ Article.getTotalWordCount = function (
 ) {
   return db
     .query(
-      `SELECT SUM("wordCount") FROM articles INNER JOIN users ON articles."userId" = users.id WHERE users.id = ${userId} AND articles.status IN ${sqlInList(
-        articleTypes
-      )} ${includeDeleted ? '' : 'AND articles."deletedAt" IS NULL'}`
+      `SELECT
+        SUM("wordCount")
+      FROM
+        articles
+      INNER JOIN
+        users
+        ON articles."userId" = users."id"
+      WHERE
+        users."id" = ${userId}
+        AND articles."status" IN ${sqlInList(articleTypes)}
+        ${includeDeleted ? '' : 'AND articles."deletedAt" IS NULL'}`
     )
     .then(data => Number(data[0][0].sum).toFixed(0));
 };
@@ -178,9 +238,17 @@ Article.getAverageWordCount = function (
 ) {
   return db
     .query(
-      `SELECT AVG("wordCount") FROM articles INNER JOIN users ON articles."userId" = users.id WHERE users.id = ${userId} AND articles.status IN ${sqlInList(
-        articleTypes
-      )} ${includeDeleted ? '' : 'AND articles."deletedAt" IS NULL'}`
+      `SELECT
+        AVG("wordCount")
+      FROM
+        articles
+      INNER JOIN
+        users
+        ON articles."userId" = users."id"
+      WHERE
+        users."id" = ${userId}
+        AND articles."status" IN ${sqlInList(articleTypes)}
+        ${includeDeleted ? '' : 'AND articles."deletedAt" IS NULL'}`
     )
     .then(data => Number(data[0][0].avg).toFixed(0));
 };
